aa70c9f5487273a6d3328bf739c53ecea54c4ffb
Xavier Roca Perez <xroca-pe@car11s2.42barcelona.com>
2024-04-03 10:30:24 +0200

    update

--- info.c
+++ info.c
@@ -6,7 +6,7 @@
 /*   By: xroca-pe <xroca-pe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 19:34:09 by xroca-pe          #+#    #+#             */
-/*   Updated: 2024/04/02 13:12:38 by xroca-pe         ###   ########.fr       */
+/*   Updated: 2024/04/03 10:29:35 by xroca-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,9 @@ static void	controls(t_main *fdf)
 		"'8' decrease altitude");
 	mlx_string_put(fdf->mlx, fdf->win, 5, 250, 0x00ff00,
 		"'5' increase altitude");
+	mlx_string_put(fdf->mlx, fdf->win, 5, 270, 0x00ff00, "'1' isometric");
+	mlx_string_put(fdf->mlx, fdf->win, 5, 290, 0x00ff00, "'2' conical");
+	mlx_string_put(fdf->mlx, fdf->win, 5, 310, 0x00ff00, "'3' parallel");
 }
 
 void	info(t_main *fdf)
